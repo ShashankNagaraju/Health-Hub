81385db5f3cfcd1d7aa171bb499767e896235ad8
Shashank N <150505337+ShashankNagaraju@users.noreply.github.com>
2024-09-18 22:56:43 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import google.generativeai as genai
+
+
+genai.configure(api_key="API_KEY")
+model=genai.GenerativeModel("gemini-pro")
+chat=model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response=chat.send_message(question,stream=True)
+    return response
+
+st.set_page_config(page_title="Sakhi")
+st.header("Sakhi - Your go to Friend")
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history']=[]
+
+input=st.text_input("Input:",key="input")
+submit=st.button("Ask the Question")
+
+if submit and input:
+    response=get_gemini_response(input)
+    st.session_state['chat_history'].append(("You",input))
+    st.subheader("Response")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot",chunk.text))
+#st.subheader("The chat history is ")
+#for role,text in st.session_state['chat_history']:
+    #st.write(f"{role}:{text}")
